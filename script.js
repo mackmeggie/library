d1fe01cca31906a5a38f1f5a3f705c66c3bdc09f
mackmeggie <mack.meggie13@gmail.com>
2022-03-14 10:38:46 -0600

    simplify displayBooks function code

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-function Books(title, author, pages, read) {
+
+let myLibrary =[];
+
+function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
     this.info = function() {
-        return(title, author, pages, read)
+        return( `${title}, ${author}, ${pages}, ${read}`)
     }
 }
+
+function addBook(x) {
+    let extrabook = myLibrary.push(x)
+}
+
+function displayBooks() {
+ //   myLibrary.forEach(element => {
+ //       console.log(element)
+ //   });
+
+    for (let book of myLibrary) {
+        console.log(book)
+    }
+    }
+
+const alanna = new Book('Alanna: The First Adventure', 'Tamora Pierce', 295, 'Read')
+const pooh = new Book('Winnie the Pooh', 'A.A. Milne', 150, 'Read');
+const narwhal = new Book('Not Quite Narwhal', 'Jessie Sima', 62, 'Read');
+
+addBook(alanna);
+addBook(pooh);
+addBook(narwhal);
+
+displayBooks();
+
+console.log()
